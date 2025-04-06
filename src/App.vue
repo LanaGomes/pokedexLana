4b1bee4868fca08a47d00cc254bb4b87b0bf8d19
Lana Gomes <lana.gomes1919@gmail.com>
2025-04-06 09:44:12 -0300

    Inicializando principais componentes

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <router-link to="/">Home</router-link>
+  <router-link to="/PokemonDetalhamento"> PokemonDetalhamento </router-link>
+  <router-view />
+</template>
---
+++ src/views/PokemonDetalhamento.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2>Pokemon detalhe</h2>
+</template>
+
+<script setup></script>
