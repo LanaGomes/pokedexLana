ad604ef8bf79fe94f0fefdd4ecbf26e4598192b5
Lana Gomes <lana.gomes1919@gmail.com>
2025-04-07 12:20:47 -0300

    Criando NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar">
+    <img src="/src/assets/pokeball.png" />
+    <img src="/src/assets/pokedex.png" />
+  </nav>
+</template>
+
+<script></script>
+
+<style>
+.navbar {
+  background-color: #d75757;
+  padding: 2rem;
+  width: 100%;
+}
+</style>
