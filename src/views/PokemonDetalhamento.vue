4c68dfe5eded723230b47e5658934e6ac3ffb978
Lana Gomes <lana.gomes1919@gmail.com>
2025-04-08 18:16:15 -0300

    Utilizando carrousel Vue

--- src/views/PokemonDetalhamento.vue
+++ src/views/PokemonDetalhamento.vue
@@ -1,21 +1,34 @@
 <template>
-  <header class="headerDetalhes">
-    <div class="nomePokemonContainer">
-      <RouterLink to="/">
-        <img id="imgSeta" src="/src/assets/setaParaEsquerda.png"
-      /></RouterLink>
-      <h4 v-if="loading">Carregando...</h4>
-      <h4 v-if="error">{{ error }}</h4>
-      <h3>
-        {{ pokemon?.name }}
-      </h3>
-    </div>
-    <h4># {{ pokemon?.id }}</h4>
-  </header>
-  <section v-for="(sprite, index) in pokemon?.sprites" class="pokemonSprites">
-    <img v-if="sprite != null" :src="sprite" class="pokemonSprite" />
-  </section>
+  <div class="backgroundPokemon">
+    <header class="headerDetalhes">
+      <div class="nomePokemonContainer">
+        <RouterLink to="/">
+          <img id="imgSeta" src="/src/assets/setaParaEsquerda.png" />
+        </RouterLink>
+        <h4 v-if="loading">Carregando...</h4>
+        <h4 v-if="error">{{ error }}</h4>
+        <h3>{{ pokemon?.name }}</h3>
+      </div>
+      <h4># {{ pokemon?.id }}</h4>
+    </header>
+
+    <!-- Carrossel de Sprites com Setas -->
+    <section class="pokemonSprites">
+      <Carousel v-bind="carouselConfig">
+        <Slide v-for="(sprite, index) in pokemonSprites" :key="index">
+          <img :src="sprite" class="pokemonSprite" />
+        </Slide>
+
+        <!-- Setas de Navegação -->
+        <template #addons>
+          <Navigation />
+          <Pagination />
+        </template>
+      </Carousel>
+    </section>
+  </div>
   <footer>
+    <!-- Tipos -->
     <div
       v-for="(item, index) in pokemon?.types"
       :key="index"
@@ -23,18 +36,24 @@
     >
       {{ item.type.name }}
     </div>
+
+    <!-- Evoluções -->
     <div class="pokemonEvolucoes">
       <h2>Evoluções</h2>
       <div v-for="(item, index) in pokemonEvolutions" :key="index">
         {{ item }}
       </div>
     </div>
+
+    <!-- Ataques -->
     <div class="pokemonAtaques">
       <h2>Ataques</h2>
       <div v-for="(item, index) in pokemon?.moves" :key="index">
         {{ item.move.name }}
       </div>
     </div>
+
+    <!-- Presença nos games -->
     <div class="pokemonGameIndices">
       <h2>Presença nos Games</h2>
       <div v-for="(item, index) in pokemon?.game_indices" :key="index">
@@ -47,7 +66,11 @@
 <script setup>
 import { RouterLink, useRoute } from "vue-router";
 import axios from "axios";
-import { ref } from "vue";
+import { ref, computed } from "vue";
+
+// Importa o carrossel e as setas
+import "vue3-carousel/carousel.css";
+import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel";
 
 const route = useRoute();
 const id = route.params.id;
@@ -58,28 +81,59 @@ const pokemon = ref(null);
 const pokemonSpecies = ref(null);
 const pokemonEvolutions = ref([]);
 
+// Configuração do carrossel
+const carouselConfig = {
+  itemsToShow: 1,
+  wrapAround: true,
+  autoplay: false,
+};
+
+// Computa os sprites válidos e únicos
+const pokemonSprites = computed(() => {
+  if (!pokemon.value || !pokemon.value.sprites) return [];
+
+  const spriteObject = pokemon.value.sprites;
+  const spriteList = [];
+
+  // Primeiro: front_default
+  if (spriteObject.front_default) {
+    spriteList.push(spriteObject.front_default);
+  }
+
+  // Adiciona outros sprites únicos
+  Object.entries(spriteObject).forEach(([key, value]) => {
+    if (
+      typeof value === "string" &&
+      value !== null &&
+      value !== spriteObject.front_default
+    ) {
+      spriteList.push(value);
+    }
+  });
+
+  return spriteList;
+});
+
+// Carrega dados da API
 async function fetchPokemon() {
   loading.value = true;
   try {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    pokemon.value = await response.data;
+    pokemon.value = response.data;
 
-    // usar os dados do pokemon para fazer uma chamada no endpoint de species
     const responseSpecies = await axios.get(pokemon.value.species.url);
-    pokemonSpecies.value = await responseSpecies.data;
+    pokemonSpecies.value = responseSpecies.data;
 
-    // depois de ter o resultado de species, chamar o endpoint de evoluition chain que está dentro do resultado de species
     const responseEvolutions = await axios.get(
       pokemonSpecies.value.evolution_chain.url
     );
-    const pokemonEvolutionsData = await responseEvolutions.data;
+    const data = responseEvolutions.data;
 
-    let arrayAuxiliar = [];
-    pokemonEvolutionsData.chain.evolves_to.forEach((evo) => {
+    const arrayAuxiliar = [];
+    data.chain.evolves_to.forEach((evo) => {
       if (pokemon.value.species.name !== evo.species.name) {
         arrayAuxiliar.push(evo.species.name);
       }
-
       if (evo.evolves_to.length > 0) {
         evo.evolves_to.forEach((evo2) => {
           if (pokemon.value.species.name !== evo2.species.name) {
@@ -90,10 +144,8 @@ async function fetchPokemon() {
     });
 
     pokemonEvolutions.value = arrayAuxiliar;
-    console.log(pokemonEvolutions.value);
   } catch (err) {
-    console.log(err);
-
+    console.error(err);
     error.value = "Erro ao carregar o Pokémon.";
   } finally {
     loading.value = false;
@@ -107,32 +159,54 @@ fetchPokemon();
 * {
   color: black;
 }
+
 .headerDetalhes {
-  color: black;
   background-color: transparent;
   padding: 0.5rem;
   display: flex;
   justify-content: space-between;
-  justify-items: center;
-  margin: 0 1rem 0 1rem;
+  margin: 0 1rem;
 }
 
 .nomePokemonContainer {
   display: flex;
-}
-
-#imgSeta {
-  margin-top: 1rem;
-  margin-right: 1rem;
-  width: 2rem;
-  height: 2rem;
   align-items: center;
 }
 
+#imgSeta {
+  margin-right: 1rem;
+  width: 2rem;
+  height: 2rem;
+}
+
+.pokemonSprites {
+  margin: 1rem auto;
+  max-width: 300px;
+}
+
 .pokemonSprite {
-  height: 20rem;
-  border: red;
-  border-width: 2px;
-  border-style: solid;
+  height: 15rem;
+  width: auto;
+  display: block;
+  margin: 0 auto;
+}
+
+.pokemonTipos {
+  display: inline-block;
+  margin: 0.3rem;
+  padding: 0.3rem 0.6rem;
+  background-color: #f1f1f1;
+  border-radius: 8px;
+  text-transform: capitalize;
+}
+
+.pokemonEvolucoes,
+.pokemonAtaques,
+.pokemonGameIndices {
+  margin-top: 2rem;
+}
+
+.backgroundPokemon {
+  background-color: rgb(120, 185, 145);
 }
 </style>
